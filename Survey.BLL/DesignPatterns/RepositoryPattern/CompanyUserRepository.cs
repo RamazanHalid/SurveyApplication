97c64dba1fabc6b6d824d4f5b78ed9ac8ba58262
Ramazan Halid <ramazan.halid.35@gmail.com>
2022-07-03 17:33:31 +0300

    CompanyUser repository added

---
+++ Survey.BLL/DesignPatterns/RepositoryPattern/CompanyUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using Survey.BLL.DesignPatterns.BaseRep;
+using Survey.DAL.ContextClasses;
+using Survey.ENTITIES.Entity;
+
+namespace Survey.BLL.DesignPatterns
+{
+    public class CompanyUserRepository:BaseRepository<CompanyUser,SurveyContext>
+    {
+
+    }
+}
