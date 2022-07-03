f33cac8f7f5ab17a76ba261827fb0a87a9459a90
Ramazan Halid <ramazan.halid.35@gmail.com>
2022-07-03 17:33:07 +0300

    CompanyUsers entity added

---
+++ Survey.ENTITIES/Entity/CompanyUser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Survey.ENTITIES.Entity
+{
+    public class CompanyUser
+    {
+        public int Id { get; set; }
+        public int CompanyId { get; set; }
+        public string Title { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string CellPhone { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Email { get; set; }
+        public int CityId { get; set; }
+        public City City { get; set; }                      
+    }
+}
