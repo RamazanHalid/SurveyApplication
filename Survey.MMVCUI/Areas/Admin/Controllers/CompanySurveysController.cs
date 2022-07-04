d3216894d69e40b377fde49c48a66f18b73af819
Ramazan Halid <ramazan.halid.35@gmail.com>
2022-07-04 11:45:48 +0300

    Company Survey Controller added

---
+++ Survey.MMVCUI/Areas/Admin/Controllers/CompanySurveysController.cs
@@ -0,0 +1,26 @@
+﻿using Survey.BLL.DesignPatterns;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Survey.MMVCUI.Areas.Admin.Controllers
+{
+    public class CompanySurveysController : Controller
+    {
+        // GET: Admin/CompanySurveys
+        CompanySurveyRepository _companySurveyRepository = new CompanySurveyRepository();
+        public ActionResult GetAll(int id)
+        {
+            var companySurvey = _companySurveyRepository.GetAll(s => s.CompanyId == id);
+            return View(companySurvey);
+        }
+
+        [HttpGet]
+        public ActionResult Add(int id)
+        {
+            return View();
+        }
+    }
+}
